7d035ff9130e2ac8fd90f55826b574337b2748d7
Moritz Rebbert <moritz.rebbert@udo.edu>
2015-12-15 01:29:25 +0100

    shivvvvvvver

---
+++ js/geometry.js
@@ -0,0 +1,14 @@
+var WIDTH = 800;
+var HEIGHT = 600;
+
+var SEED_X = WIDTH/2;
+var SEED_Y = 500;
+
+$(function(){
+    $('#wrapper').css({
+        "margin-left": (- WIDTH/2) + "px",
+        "margin-top": (-HEIGHT/2) + "px",
+        "width" : WIDTH + "px",
+        "height" : HEIGHT + "px"
+    });
+});
